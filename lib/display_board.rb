57e948d3b9717b7da021ad0ab83b37905f567a5a
Pan L <limponkimo@gmail.com>
2017-12-24 22:37:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,12 +5,6 @@ def display_board
   # puts "   |   |   "
   # puts "-----------"
   # puts "   |   |   "
-  
 
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[4]} | #{board[5]} | #{board[6]} " 
-  puts "-----------"
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   
 end
